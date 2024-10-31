ab1869c3eba2dd18523348e50e2593501cd2cd41
xvrqt <git@xvrqt.com>
2024-10-31 12:11:14 -0700

    Initial Commit: Includes Day - 00

---
+++ create_day_directory.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# NOT TRACKED BY GIT
+# Store simulation caches
+mkdir Cache
+# Store non-final renders
+mkdir Renders
+
+# TRACKED BY GIT
+# Final render stills and animations
+mkdir Final
+# Texture files used by the .blend files
+mkdir Textures
+# Environment HDRIs
+mkdir -p Textures/HDRI
